8237840b717982bc5a2d5250d456075a56283e64
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-12-11 19:12:36 -0600

    added to BestBooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import BestBooks from './BestBooks';
+import './App.css';
 import {
   BrowserRouter,
   Routes,
--- src/BestBooks.jsx
+++ src/BestBooks.jsx
@@ -1,31 +1,13 @@
-import React from 'react';
-
-class BestBooks extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      books: []
-    }
-  }
-
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
-
-  render() {
-
-    /* TODO: render all the books in a Carousel */
-
-    return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
-      </>
-    )
-  }
+function BestBooks({ books }) {
+  return (
+    <>
+      {books.length && books.map((book, idx) => (
+        <div key={idx}>
+          {book.title} is {book.status}
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default BestBooks;
