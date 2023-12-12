2969a146af809efe7036f921991b982501d83597
KAITLIN DAVIS <KAITLIN.A.DAVIS1996@GMAIL.COM>
2023-12-11 19:00:29 -0600

    worked on front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import BestBooks from './BestBooks';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
-  Route
+  Route,
+  Link,
 } from "react-router-dom";
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <Router>
-          <Header />
+const SERVER = import.meta.env.VITE_SERVER_URL;
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect (() => {
+    fetchBooks();
+  }, []);
+
+async function fetchBooks(status = 'Available') {
+let dbURL = `${SERVER}/books`;
+
+if (status) {
+  dbURL += `?status=${status}`;
+}
+
+try {
+  const response = await axios.get(dbURL);
+  setBooks(response.data);
+} catch (error) {
+  console.error(error);
+}
+}
+function handleStatusSubmit(event) {
+  event.preventDefault();
+  const status = event.target.status.value;
+  fetchBooks(status);
+}
+
+return (
+  <>
+        <BrowserRouter>
+          <nav>
+            <h1>Can of Books</h1>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+          </nav>
           <Routes>
-            <Route 
-              exact path="/"
-              element={<BestBooks />}
-            >
-            </Route>
-            {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route  exact path="/" element={
+              <div>
+                <BestBooks books={books} />
+                <h2>Filter by status</h2>
+                <form onSubmit={handleStatusSubmit}>
+                  <input name="status" />
+                  <button>ok</button>
+                </form>
+              </div>
+            } />
+            <Route path="/about" element={
+              <h1>About Page Here</h1>
+            } />
           </Routes>
-          <Footer />
-        </Router>
+        </BrowserRouter>
       </>
-    )
-  }
+)
 }
 
 export default App;
